d48364af707c557dcbb2f14c623e17d77d03bae2
Boazboy <BoazboyIII@gmail.com>
2015-03-27 19:17:34 -0400

    Added NameLabel

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MassageLabel: UILabel!
+    @IBOutlet weak var EnterNameTextFeild: UITextField!
+    @IBOutlet weak var EnterMessageTextField: UITextField!
+    
+    @IBOutlet weak var NameLabel: UILabel!
+    
+    
+    @IBOutlet weak var MailButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func SendMailButtonPressed(sender: UIButton) {
+        MassageLabel.hidden = false
+        NameLabel.hidden = false
+        MassageLabel.text = EnterMessageTextField.text
+        NameLabel.text = EnterNameTextFeild.text
+        MassageLabel.textColor = UIColor.redColor()
+        NameLabel.textColor = UIColor.blueColor()
+        EnterMessageTextField.text = ""
+        EnterNameTextFeild.text = ""
+        EnterMessageTextField.resignFirstResponder()
+        EnterNameTextFeild.resignFirstResponder()
+        
+        MailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+    
+    
+    }
 
 }
 
